12b7cd6a96ed935718346bc645bd24fec12341c6
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-09-25 10:56:43 +0300

    add tests

---
+++ Analogy.LogServer.Tests/TestForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Analogy.LogServer.Tests
+{
+    partial class TestForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProducer = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnProducer
+            // 
+            this.btnProducer.Location = new System.Drawing.Point(12, 12);
+            this.btnProducer.Name = "btnProducer";
+            this.btnProducer.Size = new System.Drawing.Size(166, 54);
+            this.btnProducer.TabIndex = 0;
+            this.btnProducer.Text = "Produce Messages";
+            this.btnProducer.UseVisualStyleBackColor = true;
+            this.btnProducer.Click += new System.EventHandler(this.btnProducer_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(199, 22);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(368, 27);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "localhost";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 76);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnProducer);
+            this.Name = "TestForm";
+            this.Text = "Analogy Log Server Message producer Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProducer;
+        private System.Windows.Forms.TextBox txtIP;
+    }
+}
+
---
+++ Analogy.LogServer.Tests/TestForm.cs
@@ -0,0 +1,48 @@
+﻿using Analogy.Interfaces;
+using Analogy.LogServer.Clients;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analogy.LogServer.Tests
+{
+    public partial class TestForm : Form
+    {
+        private bool producing;
+        private bool consuming;
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnProducer_Click(object sender, EventArgs e)
+        {
+            if (producing) return;
+            producing = true;
+            btnProducer.Enabled = false;
+            var p = new AnalogyMessageProducer($"http://{txtIP.Text}:6000", null);
+            var ai = new Dictionary<string, string> {{"some key", "some value"}};
+            for (int i = 0; i < 100000; i++)
+            {
+                await p.Log(text: "test " + i, source: "none", additionalInformation: ai, level:AnalogyLogLevel.Information).ConfigureAwait(false);
+                await Task.Delay(500).ConfigureAwait(false);
+            }
+
+            producing = false;
+            btnProducer.Enabled = true;
+        }
+
+        private void btnConsumer_Click(object sender, EventArgs e)
+        {
+            //if (consuming) return;
+            //consuming = true;
+            //btnConsumer.Enabled = false;
+            //var c = new AnalogyMessageConsumer("http://localhost:6000");
+            //await foreach (var m in c.GetMessages().ConfigureAwait(false))
+            //    richTextBox1.Text += Environment.NewLine + m;
+            //consuming = false;
+            //btnConsumer.Enabled = true;
+        }
+    }
+}
