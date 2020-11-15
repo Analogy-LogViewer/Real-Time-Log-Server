adb5aedd91ad245480d2e8480572e2097cd106c6
Lior Banai <liorbanai@gmail.com>
2020-11-15 15:55:08 +0200

    add warn log level

--- Analogy.LogServer/Utils.cs
+++ Analogy.LogServer/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
@@ -41,6 +42,7 @@ namespace Analogy.LogServer
                     return AnalogyGRPCLogLevel.Information;
                 case "WRN":
                 case "Warn":
+                case "warn":
                 case "WARN":
                 case "Warning":
                 case "WARNING":
